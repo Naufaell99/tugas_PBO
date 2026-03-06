de9efe5652dbf23dcd4e68463cdba4dbe4ae02a8
naufal <12550111917@students.uin-suska.ac.id>
2026-03-06 17:16:22 +0700

    update tugas pbo

--- soal_1_dan_2.py
+++ soal_1_dan_2.py
@@ -1,4 +1,3 @@
-# Membuat class Restaurant
 class Restaurant:
     def __init__(self, nama_restoran, jenis_masakan):
         self.nama_restoran = nama_restoran
@@ -11,23 +10,18 @@ class Restaurant:
     def buka_restoran(self):
         print(self.nama_restoran, "sekarang buka!\n")
 
-
 restaurant = Restaurant("Sanama", "Masakan Padang")
-
 
 print("Nama Restoran :", restaurant.nama_restoran)
 print("Jenis Masakan :", restaurant.jenis_masakan)
 print()
 
-
 restaurant.deskripsikan_restoran()
 restaurant.buka_restoran()
 
-
-restaurant1 = Restaurant("Sanama", "Masakan Padang")
-restaurant2 = Restaurant("Ichiban Sushi", "Masakan Jepang")
-restaurant3 = Restaurant("Pizza Hut", "Masakan Italia")
-
+restaurant1 = Restaurant("rendang", "Masakan Padang")
+restaurant2 = Restaurant("kokek", "Masakan Melayu")
+restaurant3 = Restaurant("dendeng", "Masakan Jawa")
 
 restaurant1.deskripsikan_restoran()
 print()
